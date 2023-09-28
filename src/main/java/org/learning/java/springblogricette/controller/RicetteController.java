bbfb2a13e50d1d300532226a80376a8d43416627
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-29 01:24:28 +0200

    EDIT METHOD IMPLEMENTED

--- src/main/java/org/learning/java/springblogricette/controller/RicetteController.java
+++ src/main/java/org/learning/java/springblogricette/controller/RicetteController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -44,6 +42,27 @@ public class RicetteController {
     @PostMapping("/delete/{id}")
     public String deletebyId(@PathVariable Integer id) {
         ricettaRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<Ricetta> result = ricettaRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("ricetta", result.get());
+            return "/edit";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/edit/{id}")
+    public String doEdit(@PathVariable Integer id, @ModelAttribute("ricetta") Ricetta editRicetta,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/edit";
+        }
+        ricettaRepository.save(editRicetta);
         return "redirect:/";
     }
 
